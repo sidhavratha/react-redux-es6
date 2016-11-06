d3cffa81156cd59fc4081dfa48df9fcdd101e50c
sidhavratha <sidhavratha@gmail.com>
2016-11-06 14:45:12 +0530

    redux completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import Routes from './Routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-	<Router history={browserHistory} routes={Routes} />,
+	<Provider store={store}>
+		<Router history={browserHistory} routes={Routes} />
+	</Provider>,
 	document.getElementById('app')
 );
